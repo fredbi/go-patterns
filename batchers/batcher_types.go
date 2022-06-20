b7dbd48fc3bbaf1ec2fdbf9334f83c8b4551e4a6
Frederic BIDON <fredbi@yahoo.com>
2022-06-20 16:34:06 +0200

    feat(batchers): added clonable version of batchers
    Signed-off-by: Frederic BIDON <fredbi@yahoo.com>

--- batchers/batcher_types.go
+++ batchers/batcher_types.go
@@ -5,18 +5,29 @@ import (
 )
 
 type (
-	Executor[T any] struct {
+	baseExecutor[T any] struct {
 		batchSize int
-		executor  func([]T)
-		batch     []T
 		mx        sync.Mutex
 		count     uint64
 		*options
 	}
 
+	Executor[T any] struct {
+		*baseExecutor[T]
+		batch    []T
+		executor func([]T)
+	}
+
+	PointerExecutor[T any] struct {
+		*baseExecutor[T]
+		batch    []*T
+		executor func([]*T)
+	}
+
 	Option func(*options)
 
 	options struct {
+		// TODO
 	}
 )
 
@@ -24,10 +35,9 @@ func defaultOptions() *options {
 	return &options{}
 }
 
-func NewExecutor[T any | *any](batchSize int, executor func([]T), opts ...Option) *Executor[T] {
-	e := &Executor[T]{
+func newBaseExecutor[T any](batchSize int, opts ...Option) *baseExecutor[T] {
+	e := &baseExecutor[T]{
 		batchSize: batchSize,
-		executor:  executor,
 		options:   defaultOptions(),
 	}
 
@@ -38,6 +48,20 @@ func NewExecutor[T any | *any](batchSize int, executor func([]T), opts ...Option
 	return e
 }
 
+func NewExecutor[T any | *any](batchSize int, executor func([]T), opts ...Option) *Executor[T] {
+	return &Executor[T]{
+		baseExecutor: newBaseExecutor[T](batchSize, opts...),
+		executor:     executor,
+	}
+}
+
+func NewPointerExecutor[T any](batchSize int, executor func([]*T), opts ...Option) *PointerExecutor[T] {
+	return &PointerExecutor[T]{
+		baseExecutor: newBaseExecutor[T](batchSize, opts...),
+		executor:     executor,
+	}
+}
+
 func (e *Executor[T]) cloneBatch(in []T) []T {
 	clone := make([]T, len(in))
 	copy(clone, e.batch)
@@ -45,9 +69,9 @@ func (e *Executor[T]) cloneBatch(in []T) []T {
 	return clone
 }
 
-/*
-func (e *Executor[T]) cloneElementsBatch(in []T) []T {
-	clone := make([]T, len(in))
+func (e *PointerExecutor[T]) cloneBatch(in []*T) []*T {
+	// when adopting slices of pointers, we shallow clone individual elements
+	clone := make([]*T, len(in))
 	for i, element := range in {
 		copied := *element
 		clone[i] = &copied
@@ -55,9 +79,21 @@ func (e *Executor[T]) cloneElementsBatch(in []T) []T {
 
 	return clone
 }
-*/
 
 func (e *Executor[T]) Push(in T) {
+	e.mx.Lock()
+	defer e.mx.Unlock()
+
+	e.batch = append(e.batch, in)
+
+	if len(e.batch) < e.batchSize {
+		return
+	}
+
+	e.executeClone()
+}
+
+func (e *PointerExecutor[T]) Push(in *T) {
 	e.mx.Lock()
 	defer e.mx.Unlock()
 
@@ -74,7 +110,24 @@ func (e *Executor[T]) Flush() {
 	e.executeClone()
 }
 
+func (e *PointerExecutor[T]) Flush() {
+	e.executeClone()
+}
+
 func (e *Executor[T]) executeClone() {
+	e.mx.Lock()
+	defer e.mx.Unlock()
+
+	if len(e.batch) == 0 {
+		return
+	}
+
+	e.executor(e.cloneBatch(e.batch))
+	e.count += uint64(len(e.batch))
+	e.batch = e.batch[:0]
+}
+
+func (e *PointerExecutor[T]) executeClone() {
 	e.mx.Lock()
 	defer e.mx.Unlock()
 
